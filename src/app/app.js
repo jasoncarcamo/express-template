6e6b49d28567c93d7ff8e992167ebd83d499a6c4
Jason Carcamo <jasoncarcamo30@yahoo.com>
2020-04-25 15:10:17 -0400

    Ready for creating RESTful API apps

--- src/app/app.js
+++ src/app/app.js
@@ -5,9 +5,22 @@ const helmet = require("helmet");
 const cors = require("cors");
 
 app.use(cors());
-app.use(morgan("tiny"));
+app.use(morgan((NODE_ENV === "production") ? "tiny" : "common"));
 app.use(helmet());
 
 //Routes start here
+
+app.use(function errorHandler(error, req, res, next) {
+    let response;
+
+    if (NODE_ENV === 'production') {
+      response = { error: 'Server error' };
+    } else {
+      console.error(error)
+      response = { error: error.message, object: error };
+    };
+
+    res.status(500).json(response);
+});
 
 module.exports = app;
